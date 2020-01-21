94da3c6ac7a6cd1799e264d798294b320bf168d9
Socrates Utui <socratesutui@live.com>
2020-01-21 16:25:19 +0200

    Updates

---
+++ precache-manifest.d19a3568f35dbea94f24a6dd42e48637.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0fc9f7612c7f00ccc7b673459e1f0b",
+    "url": "/mega/index.html"
+  },
+  {
+    "revision": "374c8b973d1177e51916",
+    "url": "/mega/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7dfd5aeb830fd92e975e",
+    "url": "/mega/static/js/2.91044598.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mega/static/js/2.91044598.chunk.js.LICENSE"
+  },
+  {
+    "revision": "374c8b973d1177e51916",
+    "url": "/mega/static/js/main.f8fd64b8.chunk.js"
+  },
+  {
+    "revision": "e8fdcb49d3585b9286e3",
+    "url": "/mega/static/js/runtime-main.aa7b95ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mega/static/media/logo.5d5d9eef.svg"
+  }
+]);
